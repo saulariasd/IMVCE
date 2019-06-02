7c6029482d40f2d4344f43855d6f7d4669574be1
JonnathanAndrade14 <jandradea5@est.ups.edu.ec>
2019-06-02 02:12:52 -0500

    Creacion entidades estudiante, docente, materia

---
+++ src/main/java/ec/edu/ups/entidad/Docente.java
@@ -0,0 +1,59 @@
+package ec.edu.ups.entidad;
+
+public class Docente {
+	private int id;
+	private String nombre;
+	private String telefono;
+	private String cedula;
+	private String usuario;
+	private String passwd;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+}
