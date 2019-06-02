c9ff76adbc01bbe036f09cbb16205ada07e242b8
JonnathanAndrade14 <jandradea5@est.ups.edu.ec>
2019-06-01 20:35:16 -0500

    Cambio4

--- src/main/java/ejemplo.java
+++ src/main/java/ejemplo.java
@@ -3,7 +3,14 @@ public class ejemplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(":::::     :::::");
+	   System.out.println(":::::::::::::::::");
+	   System.out.println(":::::::::::::::::");
 		System.out.println("Ruben y Hernan pero el saul mas gay");
+		  System.out.println("::::::::::::");
+		     System.out.println(":::::");
+		     System.out.println(" :::");
+		     System.out.println(" : ");
 	}
 
 }
